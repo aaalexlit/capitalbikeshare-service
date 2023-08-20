007b2c59cee5f499d2693ab1b16c12b7b1121b55
Alex Litvinov <aaalex.lit@gmail.com>
2023-08-20 20:15:10 +0200

    add prints to the integration test shell script

--- integration-test/test.sh
+++ integration-test/test.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
+echo "Starting integration test"
+
 # cd to the directory where the script is
-# and set env vars from .env
 # if it's not a github action execution
 if [[ -z "${GITHUB_ACTIONS}" ]]; then
+  echo "Switching to the script directory: $(dirname "$0")"
   cd "$(dirname "$0")"
 fi
 
 cd ..
 
+echo "Building and running docker container"
 docker compose up --build -d
 
+echo "Waiting for the service to spin up"
 sleep 30
 
+echo "Calling dockerized service"
 pipenv run pytest integration-test/test_docker.py
 
 ERROR_CODE=$?
@@ -23,4 +28,5 @@ if [ ${ERROR_CODE} != 0 ]; then
     exit ${ERROR_CODE}
 fi
 
+echo "removing docker container"
 docker compose down
